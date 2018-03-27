07139e1f6e515ed7051f14c1e17b7d4016087f25
Daehyun You <daehyun.park.you@gmail.com>
2018-03-27 02:37:11 +0000

    Change the export style

--- lmf2txt.py
+++ lmf2txt.py
@@ -16,26 +16,28 @@ args = parser.parse_args()
 
 
 afile = LMFReader(args.filename)
-print("Filename: {}".format(afile.recorded_at))
-print("Version: {}".format(afile.version))
-print("Comment: {}".format(afile.comment))
-print("Number of Channels: {}".format(afile.nchannels))
-print("Maximum Number of Hits: {}".format(afile.max_nhits))
-print("Number of Coordinates: {}".format(afile.ncoordinates))
-print("Number of Events: {}".format(len(afile)))
-print("TDC Resolution: {:.6f} ns".format(afile.to_nanosec))
-print("Start Time: {}".format(afile.time_fr))
-print("Stop Time: {}".format(afile.time_to))
+print("               Filename: {}".format(afile.recorded_at))
+print("                Version: {}".format(afile.version))
+print("                Comment: {}".format(afile.comment))
+print("     Number of Channels: {}".format(afile.nchannels))
+print(" Maximum Number of Hits: {}".format(afile.max_nhits))
+print("  Number of Coordinates: {}".format(afile.ncoordinates))
+print("       Number of Events: {}".format(len(afile)))
+print("         TDC Resolution: {:.6f} ns".format(afile.to_nanosec))
+print("             Start Time: {}".format(afile.time_fr))
+print("              Stop Time: {}".format(afile.time_to))
 
 limit = 100 if not args.full else len(afile)
 k = 1 if not args.ns else afile.to_nanosec
 for i, event in enumerate(afile):
     if not i < limit:
         break
-    print("------- #{} -------".format(event.event))
-    print("At: {:.6f} s".format(event.timestamp))
+    print("########################")
+    print("# {}".format(event.event))
+    print("########################")
+    print("                     At: {: 15.6f} s".format(event.timestamp))
     for ch, n in enumerate(event.nhits):
         fr = sum(event.nhits[:ch])
-        hits = ", ".join("{: 9.3f}".format(k*f) for f in event.hits[fr:fr+n])
-        print("Channel #{:03d} Total {:3d} Hit(s): {}".format(ch, n, hits))
+        hits = ", ".join("{: 12.3f}".format(k*f) for f in event.hits[fr:fr+n])
+        print("Channel #{:03d} {:3d} Hit(s): {}".format(ch, n, hits))
 
